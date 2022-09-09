f2e299c8d5e408888757ea009f8e3b2ccd352d79
elgrigo-garang <collinsmackey7@gmail.com>
2022-09-09 09:03:59 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main-prints text using puts
+ *
+ * Return:always return 0
+ */
+int main(void)
+	{
+		puts("/"Programming is like building a multilingual puzzle\n");
+		return (0);
+	}
