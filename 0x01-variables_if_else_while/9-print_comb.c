83dd8cb4953aa6a71ed7836e9b9fbb82ddf75084
elgrigo-garang <collinsmackey7@gmail.com>
2022-09-11 10:49:19 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 /**
- * main-prints all possible combinations 
+ * main-prints all possible combinations
  *
  * Return: always 0
  */
 int main(void)
 	{
 		int i;
-		
+
 		for (i = 48; i < 58; i++)
 		{
 			putchar (i);
-			if (n == 57)
+			if (i == 57)
 			{
 				break;
 			}
