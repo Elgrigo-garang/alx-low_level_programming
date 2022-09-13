9e9c4a06db72ba6a5c4b403fec9f4de64a363f81
elgrigo-garang <collinsmackey7@gmail.com>
2022-09-13 07:33:01 -0700

    task 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet-check code
+ *
+ * Return:return always 0
+ */
+void print_alphabet(void)
+	{
+		char c;
+
+		for (c = 'a'; c <= 'z'; c++)
+			putchar (c);
+			putchar('\n');
+	}
