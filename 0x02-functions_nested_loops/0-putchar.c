b98bf2d3bcce0082cd028016aa90506e6624bdd0
elgrigo-garang <collinsmackey7@gmail.com>
2022-09-13 06:03:48 -0700

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,21 @@
 #include "main.h"
-/**
- * main-print putchar
- *
- * Return:return 0
- */
 
+/**
+ * main-main function
+ * description:prints putchar
+ * Return:return 0
+ * _putchar-prints the output
+ */
 int main(void)
-	{
-		_putchar("_');
-		_putchar('p');
-		_putchar('u');
-		_putchar('t');
-		_putchar('c');
-		_putchar('h');
-		_putchar('a');
-		_putchar('r');
-		_putchar('\n');
-		return (0);
-	}
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	-putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
