444b274d0d89c0abe922bf35e5a659b291467c25
elgrigo-garang <collinsmackey7@gmail.com>
2022-09-14 10:00:14 -0700

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,11 +7,10 @@
  */
 int print_last_digit(int n)
 	{
-		int x;
+		int last_digit = n % 10;
 
-		if (n < 0)
-			n = -n;
-		x = n % 10;
-		_putchar(x + '0');
+		if (last_digit < n)
+			last_digit = -1;
+		_putchar(last_digit + '0');
 		return (last_digit);
 	}
