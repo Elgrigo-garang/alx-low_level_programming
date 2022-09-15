531c6ff6a0985b8379c81fed7d8e40cec7fd9924
elgrigo-garang <collinsmackey7@gmail.com>
2022-09-15 06:37:28 -0700

    task3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_numbers-prints all number 0 to 9
+ * Return:return nuthing
+ */
+void print_numbers(void)
+	{
+		int n;
+
+		for (n = 48; n < 58; n++)
+		{
+			_putchar(n);
+		}
+		_putchar(0);
+	}
