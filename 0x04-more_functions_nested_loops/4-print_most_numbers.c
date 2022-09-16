da8660eb51e61e46dc0459cadee5a6617d2a03a5
elgrigo-garang <collinsmackey7@gmail.com>
2022-09-16 02:20:41 -0700

    task 6

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,10 +8,12 @@ void print_most_numbers(void)
 	{
 		int x;
 
-		for (x = 0; x < 10; x++)
+		for (x = 0; x <= 9; x++)
 			 {
 				 if (x != 2 && x != 4)
-					 _putchar(x + '0');
+				 {
+					 _putchar((x % 10) + '0');
+				 }
 			 }
 		_putchar('\n');
 	}
--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,11 +1,33 @@
 #include "main.h"
 /**
- *
- *
+ *print_line-print a diogonal line on the terminal
+ *@n: the number of \ character to be drawn
  *
  */
 void print_line(int n)
 	{
-		int x;
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			int line, gap;
 
-
+			for (line = 0; line < n; line++)
+			{
+				for (gap = 0; gap < n; gap++)
+				{
+					if (line == gap)
+					{
+						_putchar('\\');
+					}
+					else if (gap < line)
+					{
+						_putchar(' ');
+					}
+				}
+				_putchar('\n');
+			}
+		}
+	}
