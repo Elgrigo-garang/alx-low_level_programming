ea05a247c0c8765a3af4a50f2a5b5e7f13d79c37
elgrigo-garang <collinsmackey7@gmail.com>
2022-09-16 12:59:17 -0700

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,33 +1,16 @@
+#include <stdio.h>
 #include "main.h"
+
 /**
- *print_line-print a diogonal line on the terminal
- *@n: the number of \ character to be drawn
- *
+ * print_line-prints a straight line
+ * @n:parameter
+ * Return:returns nothing
  */
 void print_line(int n)
 	{
-		if (n <= 0)
+		while (n-- > 0)
 		{
-			_putchar('\n');
+			putchar('_');
 		}
-		else
-		{
-			int line, gap;
-
-			for (line = 0; line < n; line++)
-			{
-				for (gap = 0; gap < n; gap++)
-				{
-					if (line == gap)
-					{
-						_putchar('\\');
-					}
-					else if (gap < line)
-					{
-						_putchar(' ');
-					}
-				}
-				_putchar('\n');
-			}
-		}
+		putchar('\n');
 	}
