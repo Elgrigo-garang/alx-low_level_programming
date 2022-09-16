a42166bad756a6a4c990d43a4e749f98c30710a7
elgrigo-garang <collinsmackey7@gmail.com>
2022-09-16 07:03:16 -0700

    task4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,11 +9,11 @@ void print_most_numbers(void)
 		int x;
 
 		for (x = 0; x <= 9; x++)
-			 {
-				 if (x != 2 && x != 4)
-				 {
-					 _putchar((x % 10) + '0');
-				 }
-			 }
+		{
+			if (x != 2 && x != 4)
+			{
+				_putchar((x % 10) + '0');
+			}
+		}
 		_putchar('\n');
 	}
