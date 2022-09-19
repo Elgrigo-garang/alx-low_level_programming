112ad0d6ef32196bc9dfc556dab7aa5a68135d16
elgrigo-garang <collinsmackey7@gmail.com>
2022-09-19 12:15:59 -0700

    task3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts-prints a string followed by a new line
+ * @str:string to print
+ *
+ * Description:prints a string
+ * On success:Returns no erro
+ */
+void _puts(char *str)
+	{
+		int i = 0;
+
+		while (*(str + i) != '\0')
+		{
+			putchar(*(str + i));
+			i++;
+		}
+		putchar(10);
+	}
