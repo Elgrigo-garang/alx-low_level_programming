40c333d8a4282c0fbdb1623620992f8670e1e3dd
elgrigo-garang <collinsmackey7@gmail.com>
2022-09-19 10:08:55 -0700

    task2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * int _strlen-function that returns the length of the string
+ * int_strlen-function that returns the length of the string
  * @s:string
  * Return:return length of an integer
  */
-int _strlen(char *s)
+int_strlen(char *s)
 	{
 		int len = 0;
 
