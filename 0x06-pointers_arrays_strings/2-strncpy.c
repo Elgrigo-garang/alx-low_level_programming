6101b558d1f7454dc57f97977f92488c76d7ae55
elgrigo-garang <collinsmackey7@gmail.com>
2022-09-22 12:21:10 -0700

    task2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,21 +10,13 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 	{
-		int a = 0, b = 0;
+		int i;
 
-		while (src[b])
-		{
-			b++;
-		}
-		while (a < n && src[a])
-		{
-			dest[a] = src[a];
-		}
-		while (a < n)
-		{
-			dest[a] = '\0';
-			a++;
-		}
+		for (i = 0; i < n && src[i] != '\0'; i++)
+			dest[i] = src[i];
+
+		for (; i < n; i++)
+			dest[i] = '\0';
 
 		return (dest);
 	}
