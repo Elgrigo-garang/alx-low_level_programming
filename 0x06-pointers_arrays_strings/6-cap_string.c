236e02bfe82b8515de32c40225d12848dcff2224
elgrigo-garang <collinsmackey7@gmail.com>
2022-09-22 12:41:17 -0700

    task6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,18 +3,19 @@
  * * *cap_string-changes all lowercase letters of a string to uppercase
  * @str : string
  * Return: 0
- * */
+ *
+ */
 char *cap_string(char *str)
 	{
 		int i = 0;
 
 		while (str[++i])
 		{
-			while (!(str[i] >= 'a' && str[i] <= 'z'))
+			while (!(str[i] >= 'a') && (str[i] <= 'z'))
 				i++;
 			if (str[i - 1] == ' ' ||
-					str[i - 1] == '\t'||
-					str[i - 1] == '\n'||
+					str[i - 1] == '\t' ||
+					str[i - 1] == '\n' ||
 					str[i - 1] == ',' ||
 					str[i - 1] == ';' ||
 					str[i - 1] == '.' ||
