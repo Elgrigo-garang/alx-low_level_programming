042e9891e503a59a5c33574c9459513fb0245ed2
elgrigo-garang <collinsmackey7@gmail.com>
2022-09-22 13:54:39 -0700

    task11

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,16 +7,25 @@
 char *leet(char *str)
 {
 	int i = 0;
-	int i_b;
-	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', 'T'};
 
-	while (str[++i])
+	while (str[i] != '\0')
 	{
-		for (i_b = 0; i_b <= 7; i_b++)
-		{
-			if (str[i] == leet[i_b] || str[i] - 32 == leet[i_b])
-				str[i] = i_b + '0';
-		}
+		str[i] = transform(str[i]);
+		i++;
 	}
-		return (str);
+	return (str);
 }
+/**
+ * transform-helperto map a latter encodding
+ * @x: char to be encoded
+ *
+ * Return:the encoded char
+ */
+char transform(char x)
+	{
+		char mapping_low[8] = {'o', 'l', '\0', 'e', 'a', '\0', '\0', 't'};
+		char mapping_upper[8] = {'O', 'L', '\0', 'E', 'A', '\0', '\0', 'T'};
+
+		while (i < 8)
+		{
+			if (x == mappig_low[i] 
