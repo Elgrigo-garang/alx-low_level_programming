c386bbde66676d4040328c0c8da0ea9553cb9007
elgrigo-garang <collinsmackey7@gmail.com>
2022-09-22 00:16:26 -0700

    task0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,10 +11,16 @@ char *_strcat(char *dest, char *src)
 		int len = 0, dest_len = 0;
 
 		while (dest[len++])
+		{
 			dest_len++;
+		}
 
 		for (len = 0; src[index]; len++)
+		{
 			dest[dest_len++] = src[index];
+			len++;
+		}
 
+		dest[len] = '\0';
 		return (dest);
 	}
