1ac144b57443ffd6efa166a0bb1f3c93e2fe443c
elgrigo-garang <collinsmackey7@gmail.com>
2022-09-22 10:35:35 -0700

    task5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,15 +2,18 @@
 /**
  * *string_toupper- changes all lowercase letters of a string to uppercase
  * @s : string
- * Return:s
+ * Return:string
  */
-char *string_toupper(char *s)                           {                                                               int i = 0;
+char *string_toupper(char *s)
+	{
+		int i = 0;
 
-        while (s[i++])
-        {
-                if (s[i] >= 'a' && s[i] <= 'z')                         {
-                        s[i] -= 32;
-                }
-        }
-        return (s);
-}
+		while (s[i++])
+		{
+			if (s[i] >= 'a' && s[i] <= 'z')
+			{
+				s[i] -= 32;
+			}
+		}
+		return (s);
+	}
