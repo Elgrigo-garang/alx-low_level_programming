f5ea3d594f44ee264abd54a8beb76f2377c03418
elgrigo-garang <collinsmackey7@gmail.com>
2022-09-27 12:31:30 -0700

    task6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,16 +8,10 @@ void print_chessboard(char (*a)[8])
 	{
 		int i, j;
 
-		i = 0;
-		while (i < 8)
+		for (i = 0; i < 8; i++)
 		{
-			j = 0;
-			while (j < 8)
-			{
-			_putchar(a[i][j]);
-			j++;
-			}
+			for (n = 0; n < 8; n++)
+				_putchar(a[i][j]);
+			_putchar('\0');
 		}
-		_putchar('\n');
-		i++;
 	}
