02b8ced2bb72e9baaf6da447522b7c8ab8afb860
elgrigo-garang <collinsmackey7@gmail.com>
2022-09-28 07:08:35 -0700

    task2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,12 +3,13 @@
  * _strlen_recursion-function that returns the length of a string
  * @s:pointer block of memory to fill
  *
- * Return:strlen_recursion
+ * Return:length of string
  */
 int _strlen_recursion(char *s)
 	{
 		if (*s == '\0')
 			return (0);
+		s++;
 
 		else
 			return (1 + __strlen_recursion(s + 1));
