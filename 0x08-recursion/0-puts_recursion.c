ef04771e80064d5be46fdd82984651a27a9f183b
elgrigo-garang <collinsmackey7@gmail.com>
2022-09-28 02:31:15 -0700

    task0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,13 +2,13 @@
 /**
  * _puts_recursion - prints a string, followed by a new line
  * @s: string to be printed
- * Return:always 0 (success)
+ * Return:void
  */
 void _puts_recursion(char *s)
 	{
 		if (*s == '\0')
 		{
-			_putchar('\0');
+			_putchar('\n');
 		}
 		else
 		{
