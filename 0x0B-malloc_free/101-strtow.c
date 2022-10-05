409704a839d5ecf4c4ab0d2cb659acb3f4a7a5db
elgrigo-garang <collinsmackey7@gmail.com>
2022-10-05 05:24:52 -0700

    task6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -58,15 +58,15 @@ char **strow(char *str)
 
 		if (str == NULL || str[0] == '\0')
 			return (NULL);
-		words = counts_words(str);
+		words = count_words(str);
 		if (words == 0)
 			return (NULL);
-		strings = malloc(sizeof(char *) * words + 1);
-		if (strings == 0)
+		strings = malloc(sizeof(char *) * (words + 1));
+		if (strings == NULL)
 			return (NULL);
 		for (w = 0; w < words; w++)
 		{
-			while (str[index] == " ")
+			while (str[index] == ' ')
 				index++;
 			letters = word_len(str + index);
 			strings[w] = malloc(sizeof(char) * (letters + 1));
