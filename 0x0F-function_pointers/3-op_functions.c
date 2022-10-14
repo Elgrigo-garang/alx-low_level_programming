e120306ed1fdc044134e2dc36243d2af3dd62a4d
elgrigo-garang <collinsmackey7@gmail.com>
2022-10-13 19:29:08 -0700

    task4

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+/**
+ * op_add-calculates sum of integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+	{
+		return (a + b);
+	}
+/**
+ * op_sub-calculates difference of two integers
+ * @a : first integer
+ * @b : second integer
+ *
+ * Return: difference of a and b
+ */
+int op_sub(int a, int b)
+	{
+		return (a - b);
+	}
+/**
+ * op_mul-calculates the products of two itegers
+ * @a : first integer
+ * @b : second integer
+ *
+ * Return: product of two integers
+ */
+int op_mul(int a, int b)
+	{
+		return (a * b);
+	}
+/**
+ * op_div-calculates division of two integers
+ * @a :first integer
+ * @b:second integer
+ *
+ * Return:the results of division of a and b
+ */
+int op_div(int a, int b)
+	{
+		return (a / b);
+	}
+/**
+ * op_mol- calculates the remender of two integer after division
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return:the remender of a and b
+ */
+int op_mol(int a, int b)
+	{
+		return (a % b);
+	}
