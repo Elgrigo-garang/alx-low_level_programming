e858d9e4a1ab469100211d10350d3170ca49a59c
Elgrigo-garang <collinsmackey7@gmail.com>
2022-10-31 05:44:38 -0700

    fix

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,23 +1,23 @@
 #include "main.h"
 /**
- * set_bit- returns the value of a bit
+ * get_bit- returns the value of a bit
  * at a given index
  * @n:unsigned long int input
  * @index: index of a bit
  *
  * Return:value of the bit
  */
-int set_bit(unsigned long int *n, unsigned int index)
+int get_bit(unsigned long int n, unsigned int index)
 	{
 		unsigned int i;
 
 		if (n == 0 && index < 64)
 			return (0);
-		for (i = 0; i <= 63; i >>= 1, i++)
+		for (i = 0; i <= 63; n >>= 1, i++)
 		{
 			if (index == 1)
 			{
-				return (n && 1);
+				return (n & 1);
 			}
 		}
 		return (-1);
