f0e5afe943801d248fbed6b5635604b0bbf15fd9
Elgrigo-garang <collinsmackey7@gmail.com>
2022-11-02 17:28:08 -0700

    task1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,6 @@
 #include "main.h"
-/*
- * create_file- function that  creats a file
+/**
+ * create_file - function that  creats a file
  * @filename:filename
  * @text_content:content writen in the file
  *
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 		if (!filename)
 			return (-1);
 
-		fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+		fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 		if (fd == -1)
 			return (-1);
