d6c2601ea334f5df8add3075601475f188d7d562
Elgrigo-garang <collinsmackey7@gmail.com>
2022-11-05 02:53:50 -0700

    task5

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,51 +1,72 @@
-#include "main.h" 
-  
- /** 
-  * main - copies contents of a file to another 
-  * @argv: argument vector 
-  * @argc: argument count 
-  * Return: 0 on success 
-  */ 
- int main(int argc, char **argv) 
- { 
-         int fd1, fd2, wr, tmp = 1024, cl1, cl2; 
-         char *buff; 
-  
-         if (argc != 3) 
-         { 
-                 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"); 
-                 exit(97); 
-         } 
-  
-         fd1 = open(argv[1], O_RDONLY); 
-         fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 00664); 
-  
-         while (tmp == 1024) 
-         { 
-                 buff = malloc(sizeof(char) * 1024); 
-                 tmp = read(fd1, buff, 1024); 
-                 if (buff == NULL || fd1 < 0 || tmp < 0) 
-                 { 
-                         dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]); 
-                         free(buff); 
-                         exit(98); 
-                 } 
-  
-                 wr = write(fd2, buff, tmp); 
-                 if (wr < 0 || fd2 < 0) 
-                 { 
-                         dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]); 
-                         free(buff); 
-                         exit(99); 
-                 } 
-                 free(buff); 
-         } 
-         cl1 = close(fd1); 
-         cl2 = close(fd2); 
-         if (cl1 == -1 || cl2 == -1) 
-         { 
-         dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", (cl1 == -1) ? fd1 : fd2); 
-                 exit(100); 
-         } 
-         return (0); 
- }
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * error_file - checks if files can be opened.
+ * @file_from: file_from.
+ * @file_to: file_to.
+ * @argv: arguments vector.
+ * Return: no return.
+ */
+void error_file(int file_from, int file_to, char *argv[])
+{
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+}
+
+/**
+ * main - check the code for Holberton School students.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int file_from, file_to, err_close;
+	ssize_t nchars, nwr;
+	char buf[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
+	}
+
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_file(file_from, file_to, argv);
+
+	nchars = 1024;
+	while (nchars == 1024)
+	{
+		nchars = read(file_from, buf, 1024);
+		if (nchars == -1)
+			error_file(-1, 0, argv);
+		nwr = write(file_to, buf, nchars);
+		if (nwr == -1)
+			error_file(0, -1, argv);
+	}
+
+	err_close = close(file_from);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+
+	err_close = close(file_to);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	return (0);
+}
